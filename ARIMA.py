479fd065e886ebbc754eef201fca3997d6964f88
Matthew Peplinski <matthewpeplinski364@gmail.com>
2025-05-14 10:05:34 -0500

    Update ARIMA.py
    Added root mean squared error for measuring model performance, and added a seasonal arima model

--- ARIMA.py
+++ ARIMA.py
@@ -4,60 +4,51 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from sklearn.metrics import root_mean_squared_error
 from scipy.stats import boxcox
 from scipy.special import inv_boxcox
 
 
-def arima_select(feature, filePath, testFile):
-    weatherDat = pd.read_csv(filePath)
-
+def arima_select(feature, trainFile, testFile):
+    weatherDat = pd.read_csv(trainFile)
     featureData = weatherDat.loc[:, ['NAME', 'DATE', feature]].dropna()
     featureData['DATE'] = pd.to_datetime(featureData['DATE'])
-    plt.plot(featureData['DATE'], featureData[feature], c="blue")
-    plt.show()
+    #plt.plot(featureData['DATE'], featureData[feature], c="blue")
+    #plt.show()
 
 
-    #based on this plot we see that the data seems to have a trend which needs to de removed
-    result = adfuller(featureData[feature])
-    print('ADF Statistic:', result[0])
-    print('p-value:', result[1])
-
-
-    plot_acf(featureData[feature], lags=48)
-    plot_pacf(featureData[feature], lags=48)
-    plt.show()
+    #plot_acf(featureData[feature], lags=500)
+    #plot_pacf(featureData[feature], lags=75)
+    #plt.show()
     #significant out to 28 lags according to pacf
 
     """
     #Uncomment to check differencing required to make data stationary
-    featureDataD1 = featureData
-    featureDataD1[feature] = featureDataD1[feature].diff().dropna()
-    plt.plot(featureDataD1['DATE'], featureDataD1[feature])
-    plt.show()
     
-    result = adfuller(featureDataD1[feature])
+    #based on this plot we see that the data seems to have a trend which needs to de removed
+    result = adfuller(featureData[feature])
+    print('ADF Statistic:', result[0])
+    print('p-value:', result[1])
+  
+    result = adfuller(EauClaireData[feature])
     print('ADF Statistic:', result[0])
     print('p-value:', result[1])
     """
 
-
-    #using boxcox to normalize varience
-    #featureData[feature], lam = boxcox(featureData[feature])
     testData = pd.read_csv(testFile)
     testData['DATE'] = pd.to_datetime(testData['DATE'])
+    trainData = featureData.loc[1:, ['DATE', feature]]
+    testData = testData.loc[1:, ['DATE', feature]]
 
-    # remaining work is to rein in parameter tuning
-    model = ARIMA(featureData[feature], order=(28,1,12)).fit()
-    boxcox_forecast = model.forecast(len(testData))
+    # remaining work is to reign in parameter tuning
+    model = ARIMA(trainData[feature], order=(24,1,12)).fit()
+    forecast_base = model.forecast(len(testData))
 
-    #undoing the boxcox transformation messes things up
-    #forecasts = inv_boxcox(boxcox_forecast, lam)
-
-    plt.plot(featureData['DATE'], featureData[feature], c="blue")
-    plt.plot(testData['DATE'], testData[feature], c="red")
-    plt.plot(testData['DATE'], boxcox_forecast, c="green")
-    plt.show()
+    modelSeasonal = ARIMA(trainData[feature], order=(15,1,12), seasonal_order = (1,1,2,24)).fit()
+    forecast_seasonal = modelSeasonal.forecast(len(testData))
 
     #check residual sum of squares
-    #score = np.sum((boxcox_forecast - testData[feature])**2)
-    #print(score)
+    score_base =  root_mean_squared_error(testData[feature], forecast_base)
+    score_seasonal = root_mean_squared_error(testData[feature], forecast_seasonal)
+
+    return forecast_base, score_base, forecast_seasonal, score_seasonal
